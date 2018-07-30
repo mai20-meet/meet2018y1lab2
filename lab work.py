fc6473baf94c72ddc827af8fa5190a95d3b62ba0
mai20-meet <mai20@meet.mit.edu>
2018-07-30 03:33:11 -0400

    Stopping here

---
+++ lab work.py
@@ -0,0 +1,67 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> a = 5
+>>> b = 10
+>>> a = b
+>>> b =10
+>>> a =
+SyntaxError: invalid syntax
+>>> a
+10
+>>> b
+10
+>>> c = 5
+>>> c = b
+>>> b
+10
+>>> c = 5
+>>> a=5
+>>> b = 10
+>>> c = a
+>>> a = b
+>>> b
+10
+>>> >>> a = b
+SyntaxError: invalid syntax
+>>> a
+10
+>>> c
+5
+>>> 
+>>> b = c
+>>> a
+10
+>>> b
+5
+>>> c
+5
+>>> four = '4'
+>>> print (four*3)
+444
+>>> five = 4
+>>> print (five)
+4
+>>> print (five*3)
+12
+>>> my_name = 'student'
+>>> print ("hi," + myname')
+	   
+SyntaxError: EOL while scanning string literal
+>>> >>> my_name = ‘student’
+>>> print(“Hi,” + my_name)
+	   
+SyntaxError: invalid syntax
+>>> 
+	   zame = ‘student’
+>>> print(“Hi,” + my_name)
+	   
+SyntaxError: unexpected indent
+>>> 
+	   
+>>> my_name = 'student'
+	   
+>>> print ("hi," + my_name)
+	   
+hi,student
+>>> 
